94ceef7837c79b62e0b613c50163847b598bca6c
RachelFurlong-dev <racheldesignuk@gmail.com>
2026-01-14 17:20:25 +0000

    Update configuration settings

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-o@*=zi@=$7%&_++a3!yf6di36$&#&x5ev9(p6q84o@^hc#p%%y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
